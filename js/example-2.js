51735b69139d2b7794dfa6f0837ed03dcea9ed99
billvsme <994171686@qq.com>
2016-01-27 22:32:52 +0800

    Complete react doc tutorial example

--- js/example-2.js
+++ js/example-2.js
@@ -1,42 +1,83 @@
+var data = [
+    {author: "Pete Hunt", text: "This is one comment."},
+    {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var Comment = React.createClass({
+    rawMarkup: function() {
+        var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+        return {__html: rawMarkup};
+    },
+
     render: function() {
         return (
             <div className="comment">
             <h2 className="commentAuthor">
                 {this.props.author}
             </h2>
-            {this.props.children}
+            <span dangerouslySetInnerHTML={this.rawMarkup()}></span>
             </div>
         );
     }
 });
+
 var CommentList = React.createClass({
     render: function() {
+        var commentNodes = this.props.data.map(function (comment)   {
+            return (
+                <Comment author={comment.author}>
+                    {comment.text}
+                </Comment>
+            );
+        });
         return (
             <div className="commentList">
-                <Comment author="Pete Hunt">This is one comment</Comment>
-                <Comment author="Jordan Walke">This is another comment</Comment>
+                {commentNodes}
             </div>
         );
     }
 });
 
 var CommentForm = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var author = this.refs.author.value.trim();
+        var text = this.refs.text.value.trim();
+        if (!text || !author) {
+            return;
+        }
+        this.props.onCommentSubmit({author: author, text: text});
+        this.refs.author.value = '';
+        this.refs.text.value = '';
+    },
     render: function() {
         return (
-            <div className="commentForm">
-                Hello, CommentForm.
-            </div>
+            <form action="" className="commentForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" ref="author"/>
+                <input type="text" placeholder="Say something..." ref="text"/>
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 });
+
 var CommentBox = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.setState({data: data})
+    },
+    handleCommentSubmit: function(comment) {
+        data.push(comment);
+        this.setState({data: data})
+    },
     render: function() {
         return (
             <div className="CommentBox">
                 <h1>Comments</h1>
-                <CommentList />
-                <CommentForm />
+                <CommentList data={this.state.data}/>
+                <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
             </div>
         );
     }
